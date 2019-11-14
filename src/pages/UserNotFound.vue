0268476cef85e56c161ce2c2b6135260531add71
LEMSantos <lucas_m-santos@hotmail.com>
2019-11-13 23:46:26 -0300

    Add - user not found page

---
+++ src/pages/UserNotFound.vue
@@ -0,0 +1,37 @@
+<template>
+	<q-page
+		class="window-height window-width column justify-center items-center relative-position q-pa-md"
+	>
+		<q-btn
+			class="absolute-top-left q-ma-md"
+			icon="fas fa-arrow-left"
+			text-color="primary"
+			round
+			flat
+			@click="$router.push('/')"
+		/>
+
+		<q-img
+			src="statics/img/not_found.png"
+			class="image-size q-mb-md"
+		/>
+
+		<p
+			class="text-h2 text-center"
+		>
+			User not found...
+		</p>
+	</q-page>
+</template>
+
+<script>
+export default {
+	// name: 'PageName',
+}
+</script>
+
+<style scoped>
+.image-size {
+	max-width: 200px;
+}
+</style>
